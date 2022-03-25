095fa8b9094b40df9a41eae525dc6df9c1f0db94
toddpeters <tpeters@shaw.ca>
2022-03-24 20:28:52 -0600

    a

--- pages/index.js
+++ pages/index.js
@@ -3,6 +3,11 @@ import styles from '../styles.module.css'
 export default function Home() {
   return (
     <div className={styles.hello}>
+      <head>  
+      <title>Quadrito</title>
+        <meta property="og:title" content="My page title" key="title" />
+
+      </head>
       <p>Welcome to Quadrito</p>
     </div>
   )
